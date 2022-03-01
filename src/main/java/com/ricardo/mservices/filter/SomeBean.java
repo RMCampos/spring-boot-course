aa8f4ce299452e9229d536bfc01fa866cdb3d49a
Ricardo Campos <ricardo@ricardocampos.blog>
2022-03-01 16:45:21 -0300

    Dynamic filtering example

--- src/main/java/com/ricardo/mservices/filter/SomeBean.java
+++ src/main/java/com/ricardo/mservices/filter/SomeBean.java
@@ -1,21 +1,16 @@
 package com.ricardo.mservices.filter;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonFilter;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@JsonIgnoreProperties(value={"field1", "field2"})
+@JsonFilter("SomeBeanFilter")
 public class SomeBean {
 
-    @JsonIgnore
     private String field1;
-
-    @JsonIgnore
     private String field2;
-
     private String field3;
 
     public SomeBean(String f1, String f2, String f3) {
